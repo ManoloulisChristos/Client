9104c268c7d6577daf9b371bc236031c3aab2d36
Christos Manoloulis <christosmanoloulis@gmail.com>
2024-06-30 11:07:08 +0300

    trying scroll-snap

--- src/components/GlobalTest.jsx
+++ src/components/GlobalTest.jsx
@@ -6,19 +6,102 @@ import { createToast } from '../features/toast/toastsSlice';
 import usePersist from '../hooks/usePersist';
 
 const GlobalTest = () => {
-  const [persist, setPersist] = usePersist();
-  const [session, setSession] = useSession();
-  console.log(persist, 'persits');
-  console.log(session, 'session');
+  const [visible, setVisible] = useState(0);
+
+  const boxRef = useRef(null);
+  const contRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: contRef.current,
+      threshold: [0.6],
+    };
+
+    const callback = (entry) => {
+      console.log(entry);
+      console.log('intersecting', entry[0].isIntersecting);
+      console.log('intersectionRation', entry[0].intersectionRatio);
+      entry[0].target.classList.toggle('in-view', entry[0].isIntersecting);
+      // if (entry[0].isIntersecting) {
+      //   entry[0].target.classList.toggle('in-view', entry[0].isIntersecting);
+      // } else {
+      //   entry[0].target.classList.toggle('in-view', entry[0].isIntersecting);
+      // }
+    };
+    const io = new IntersectionObserver(callback, options);
+    io.observe(boxRef.current);
+  }, []);
+
+  const scrollTo = () => {
+    const delta = Math.abs(
+      boxRef.current.offsetLeft - contRef.current.offsetLeft
+    );
+
+    const pos =
+      delta - contRef.current.clientWidth / 2 + boxRef.current.clientWidth / 2;
+    contRef.current.scrollTo(pos, 0);
+  };
+
   return (
-    <div>
-      <dl>
-        <dt>Genres</dt>
-        <dd>
-          A free, open source, cross-platform, graphical web browser developed
-          by the Mozilla Corporation and hundreds of volunteers.
-        </dd>
-      </dl>
+    <div className='test'>
+      <button
+        onClick={() =>
+          console.log(
+            'port',
+            contRef.current.offsetLeft,
+            'box',
+            boxRef.current.offsetLeft,
+            'bounding Cont',
+            contRef.current.getBoundingClientRect(),
+            'scrollWidth',
+            contRef.current.scrollWidth,
+            'clientWidth',
+            contRef.current.clientWidth,
+            'offsetWidth',
+            contRef.current.offsetWidth,
+            'boxWidth',
+            boxRef.current.offsetWidth
+          )
+        }>
+        CLICK
+      </button>
+      <button onClick={scrollTo}>Scroll</button>
+
+      <div ref={contRef} className='test__container' tabIndex='-1'>
+        <div className='test__snap'>
+          <div className='test__test' data-visible=''></div>
+        </div>
+        <div className='test__snap'>
+          <div
+            // ref={boxRef}
+            style={{ backgroundColor: 'yellow' }}
+            className='test__test'></div>
+        </div>
+        <div className='test__snap'>
+          <div
+            // ref={boxRef}
+            style={{ backgroundColor: 'green' }}
+            className='test__test'></div>
+        </div>
+        <div className='test__snap'>
+          <div
+            ref={boxRef}
+            style={{ backgroundColor: 'blue' }}
+            className='test__test'></div>
+        </div>
+        <div className='test__snap'>
+          <div
+            // ref={boxRef}
+            style={{ backgroundColor: 'indigo' }}
+            className='test__test2'></div>
+        </div>
+        <div className='test__snap'>
+          <div
+            // ref={boxRef}
+            style={{ backgroundColor: 'orange' }}
+            className='test__test'></div>
+        </div>
+      </div>
     </div>
   );
 };
