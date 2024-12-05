3f0fadf650b3c06afe83f1f293518aec710e3b48
Christos Manoloulis <christosmanoloulis@gmail.com>
2024-12-05 08:17:55 +0200

    pame mwre ligo2

--- src/components/GlobalTest.jsx
+++ src/components/GlobalTest.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import './GlobalTest.scss';
 import useSession from '../hooks/useSession';
 import { useDispatch } from 'react-redux';
@@ -9,11 +9,23 @@ import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 const GlobalTest = () => {
   const animRef = useRef();
-
+  useLayoutEffect(() => {
+    console.log('LayoutEffect', animRef.current.getBBox());
+  }, []);
+  useEffect(() => {
+    console.log('Effect', animRef.current.getBBox());
+  }, []);
   return (
     <div className='test'>
-      <button onClick={() => animRef.current.beginElement()}>Play</button>
-      <svg version='1.1' viewBox='0 0 100 100' width={1000} height={1000}>
+      <button onClick={() => console.log(animRef.current.getBBox())}>
+        bbox
+      </button>
+      <button
+        onClick={() => console.log(animRef.current.getBoundingClientRect())}>
+        rect
+      </button>
+
+      {/* <svg version='1.1' viewBox='0 0 100 100' width={1000} height={1000}>
         <defs>
           <filter id='spotlight'>
             <feSpecularLighting
@@ -26,6 +38,16 @@ const GlobalTest = () => {
           </filter>
         </defs>
         <rect width='100%' height='100%' filter='url(#spotlight)'></rect>
+      </svg> */}
+      <svg version='1.1' viewBox='0 0 100 100' width={1000} height={1000}>
+        <g ref={animRef}>
+          <rect
+            id='test-rect'
+            // transform='scale(.5) translate(20 20)'
+            width='100%'
+            height='100%'
+            filter='url(#spotlight)'></rect>
+        </g>
       </svg>
     </div>
   );
